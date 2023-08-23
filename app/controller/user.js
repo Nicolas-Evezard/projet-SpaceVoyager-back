51c0d706483f5b2c0bb76c648a3ea0922ed2e252
Kerdjadjreda <reda.kerdjadj@oclock.school>
2023-08-23 15:41:53 +0200

    feat: router-user-delete-one
    add controller & model on this feature

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,7 +5,25 @@ const userController = {
   async register(req, res, next) {},
   async getOne(req, res, next) {},
   async modify(req, res, next) {},
-  async delete(req, res, next) {},
+
+  
+    /**
+  * Récupère et supprime un user
+  *@param {*} req
+  * @param {*} res
+  * @param {*} next
+  */
+  async delete(req, res, next) {
+    const { error, result } = await userDatamapper.delete();
+    if (error) {
+      // si j'ai une erreur => next(error)
+      next(error);
+    } else {
+      // si tout va bien
+      res.json(result);
+    }
+  },
+
   async login(req, res, next) {},
 };
 
--- app/model/user.js
+++ app/model/user.js
@@ -2,6 +2,34 @@ const client = require("../service/dbPool");
 const debug = require("debug")("model"); // ("model") est le namespace utilisé dans ce fichier
 const APIError = require("../service/APIError");
 
-const userDatamapper = {};
+const userDatamapper = {
+
+    async delete() {
+        const sqlQuery = `SELECT * FROM web.delete_user($1);`;
+        const values = [id];
+        let result;
+        let error;
+    
+        try {
+          const response = await client.query(sqlQuery, values);
+    
+          // je teste pour savoir si au moins une ligne a été retournée
+          //! ATTENTION GESTION D'ERREUR EN BOOLEAN A REVOIR
+          if (response.rows == false) {
+            // aucun user n'a été trouvé
+            error = new APIError("Aucun user n'a été trouvée", 404);
+          } else {
+            // je place la réponse dans result
+            result = true;
+          }
+        } catch (err) {
+          // je crèe une erreur 500
+          error = new APIError("Erreur interne au serveur", 500, err);
+        }
+    
+        // je retourne le résultat et l'erreur éventuelle
+        return { error, result };
+      },
+};
 
 module.exports = userDatamapper;
