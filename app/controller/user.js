120f953392e6537ad0cb6fb196c2781827b9bc81
NicolasCorroyez <corroyez.nicolas@gmail.com>
2023-08-24 18:21:32 +0200

    fix : Error handle and quick fixies
    user controller : require Api Error
    user model : Error handle
    index router : Error middleware
    user SQL : added OR REPLACE on insert

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,12 +1,148 @@
 const { userDatamapper } = require("../model");
 const debug = require("debug")("controller");
+const jwt = require("jsonwebtoken");
+const APIError = require("../service/APIError");
+
+const saltRounds = 10;
 
 const userController = {
-  async register(req, res, next) {},
-  async getOne(req, res, next) {},
-  async modify(req, res, next) {},
-  async delete(req, res, next) {},
-  async login(req, res, next) {},
+  /**
+   * Méthode pour register un user
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  async register(req, res, next) {
+    try {
+      // Je sépare le password de userData des infos reçu depuis le client
+      const { password, ...userData } = req.body;
+
+      // Générer un salt pour le cryptage, là je décide de couper le password en 10
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      // J'utilise le spread operator pour créer un nouvel objet password qui prend en compte les propriété de l'objet userData
+      const { error, result } = await userDatamapper.addOne({
+        ...userData,
+        password: hashedPassword,
+      });
+
+      if (error) {
+        // Si j'ai une erreur => next(error)
+        next(error);
+      } else {
+        // Si tout va bien
+        res.json(result);
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  /**
+   * Méthode pour récupérer un user par son id
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  async getOne(req, res, next) {
+    const { error, result } = await userDatamapper.getOne(req.params.id);
+
+    if (error) {
+      next(error);
+    } else {
+      res.json(result);
+    }
+  },
+
+  /**
+   * Récupère et modify un user
+   *@param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  async modifyOne(req, res, next) {
+    const user = req.body;
+    user.id = req.params.id;
+    const { error, result } = await userDatamapper.modifyOne(user);
+    if (error) {
+      // si j'ai une erreur => next(error)
+      next(error);
+    } else {
+      // si tout va bien
+      res.json(result);
+    }
+  },
+
+  /**
+   * Récupère et supprime un user
+   *@param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  async deleteOne(req, res, next) {
+    const { error, result } = await userDatamapper.deleteOne();
+    if (error) {
+      // si j'ai une erreur => next(error)
+      next(error);
+    } else {
+      // si tout va bien
+      res.json(result);
+    }
+  },
+
+  /**
+   * Méthode pour login un user
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  async login(req, res, next) {
+    debug(req.body);
+    const { error, result } = await userDatamapper.checkUser(req.body);
+    if (error) {
+      // si j'ai une erreur => next(error)
+      debug(error);
+      next(error);
+    } else {
+      // si tout va bien
+      if (result) {
+        console.log("##### results");
+        debug(result);
+        debug(req.body);
+        const match = await bcrypt.compare(req.body.password, result.password);
+        if (match) {
+          // j'enregistre les informations de l'utilisateur dans la session
+          req.session.user = result;
+          delete req.session.user.password;
+          debug(req.session.user);
+
+          // je génère un token à partir des informations de mon utilisateur et du secret
+          const token = jwt.sign(
+            {
+              user: req.session.user,
+            },
+            process.env.JWT_SECRET
+          );
+
+          // je retourne le token
+          res.json({
+            logged: true,
+            id: req.session.user.id,
+            firstname: req.session.user.firstname,
+            token: token,
+          });
+        } else {
+          // le couple email/mot de passe est incorrect
+          const err = new APIError("Mot de passe ou mail incorrect", 400);
+          next(err);
+        }
+      } else {
+        // le couple email/mot de passe est incorrect
+        const err = new APIError("Mot de passe ou mail incorrect", 400);
+        next(err);
+      }
+    }
+  },
 };
 
 module.exports = userController;
--- app/model/user.js
+++ app/model/user.js
@@ -2,6 +2,136 @@ const client = require("../service/dbPool");
 const debug = require("debug")("model"); // ("model") est le namespace utilisé dans ce fichier
 const APIError = require("../service/APIError");
 
-const userDatamapper = {};
+const userDatamapper = {
+  async addOne(user) {
+    const sqlQuery = `
+        SELECT * FROM web.insert_user($1)
+        `;
+    const values = [user];
+    console.log(user);
+    let result;
+    let error;
+    try {
+      // j'envoie ma requête à ma BDD
+      const response = await client.query(sqlQuery, values);
+      // je place la réponse dans la variable result
+      result = response.rows;
+      debug(result);
+    } catch (err) {
+      debug(err);
+      // je crée une erreur 500
+      error = new APIError("Erreur interne au serveur", 500);
+    }
+
+    // je retourne le résultat et l'erreur éventuelle
+    return { error, result };
+  },
+
+  async deleteOne() {
+    const sqlQuery = `SELECT * FROM web.delete_user($1);`;
+    const values = [id];
+    let result;
+    let error;
+
+    try {
+      const response = await client.query(sqlQuery, values);
+
+      // je teste pour savoir si au moins une ligne a été retournée
+      //! ATTENTION GESTION D'ERREUR EN BOOLEAN A REVOIR
+      if (response.rows == false) {
+        // aucun user n'a été trouvé
+        error = new APIError("Aucun user n'a été trouvée", 404);
+      } else {
+        // je place la réponse dans result
+        result = true;
+      }
+    } catch (err) {
+      // je crèe une erreur 500
+      error = new APIError("Erreur interne au serveur", 500, err);
+    }
+
+    // je retourne le résultat et l'erreur éventuelle
+    return { error, result };
+  },
+  async getOne(userId) {
+    // I prepare my request SQL
+    const sqlQuery = `
+        SELECT * FROM web.get_one_user($1)
+        `;
+    // I put values in an array to use the parametrylized request system
+    const values = [userId];
+    let result;
+    let error;
+    try {
+      const response = await client.query(sqlQuery, values);
+
+      result = response.rows;
+
+      debug(result);
+    } catch (err) {
+      debug(err);
+
+      // I create a new error 500
+      error = new APIError("Internal error server", 500);
+    }
+    // I return the result of the potential error
+    return { error, result };
+  },
+
+  async modifyOne(userInfo) {
+    const sqlQuery = `
+        SELECT * FROM web.update_user($1)
+        `;
+    const values = [userInfo];
+    let result;
+    let error;
+    try {
+      const response = await client.query(sqlQuery, values);
+
+      result = response.rows;
+
+      debug(result);
+    } catch (err) {
+      debug(err);
+
+      // I create a new error 500
+      error = new APIError("Internal error server", 500);
+    }
+    // I return the result of the potential error
+    return { error, result };
+  },
+
+  async checkUser(user) {
+    // je prépare ma requête SQL
+    const sqlQuery = `
+          SELECT * FROM web.check_user($1);
+      `;
+    const values = [user];
+
+    let result;
+    let error;
+
+    try {
+      // j'envoie ma requête à ma BDD
+      const response = await client.query(sqlQuery, values);
+      if (response.rows[0].mail == null) {
+        error = new APIError("Email ou mot de passe incorrect", 400);
+      } else {
+        // je place la réponse dans result
+        result = response.rows[0];
+        console.log(result);
+
+        debug(result);
+      }
+    } catch (err) {
+      debug(err);
+      // je crèe une erreur 500
+      error = new APIError("COUCOU Erreur interne au serveur", 500);
+    }
+
+    // je retourne le résultat et l'erreur éventuelle
+    return { error, result };
+  },
+};
 
 module.exports = userDatamapper;
